a0c0909550ef9bb61a34af5b035fae2947511cba
Giovanni86 <giovanni.borda@gmail.com>
2016-02-24 16:34:13 -0500

    input text y boton de submit

--- index.php
+++ index.php
@@ -26,7 +26,30 @@
 	<title>Aprendo con Colombia aprende</title>
 </head>
 <body>
-	<h1>hola mundo</h1>
+	<nav class="navbar">
+      <div class="container">
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">F</a></li>
+            <li><a href="#about">T</a></li>
+            <li><a href="#contact">Y</a></li>
+            <li><a href="#contact">R</a></li>
+            <li><a href="#contact">Fl</a></li>
+            <li><a href="#contact">P</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 
+    <div class="container">
+    <div class="jumbotron">
+    	<form class="form-inline">
+		  <div class="form-group">
+		    <input type="text" class="form-control" id="search" placeholder="ingrese la palabra">
+		  </div>
+		  <button type="submit" class="btn btn-default">buscar</button>
+		</form>
+	</div>  	
+    </div><!-- /.container -->
 </body>
 </html>
