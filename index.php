f52dce8ed8107d1b3fc958f94ee795649f474b5e
Giovanni86 <giovanni.borda@gmail.com>
2016-02-24 15:56:50 -0500

    archivos de estilos y js

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<!--<link href="css/bootstrap.css" rel="stylesheet" type="text/css">-->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	
+	<link href="css/base.css" rel="stylesheet" type="text/css">
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script src="js/jquery/jquery.min.js"></script>
+	
+	<!--resise window-->
+	<!--<script src="js/bootstrap.js"></script>-->
+	<script src="js/bootstrap.js"></script>
+
+	<script type="text/javascript">
+	</script>
+	<title>Aprendo con Colombia aprende</title>
+</head>
+<body>
+	<h1>hola mundo</h1>
+
+</body>
+</html>
